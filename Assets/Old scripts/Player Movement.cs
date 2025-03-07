93af3f59979f48cd9bebc709cc2845aa729ff10d
Harman420-eee <99343500+Harman420-eee@users.noreply.github.com>
2025-03-07 01:59:08 -0500

    Testing movement.
    Need to fix weird movement during sidesteps. Left right movement works fine.

--- Assets/Old scripts/Player Movement.cs
+++ Assets/Old scripts/Player Movement.cs
@@ -8,71 +8,62 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float PlayerSpeed = 6f;
     [SerializeField] Rigidbody Playerbody;
+    [SerializeField] Transform Enemy;
     [SerializeField] Transform cam;
-    [SerializeField] GameObject enemysensor;
-    EnemySensor enemysensorscript;
-    Transform enemytransform;
     Quaternion player_rotation;
     Vector3 directiontoenemy;
     public Vector3 direction;
-    float TurnSmoothTime = 0.1f;
-    float turnsmoothvelocity;
     public float targetangle;
     // Start is called before the first frame update
     void Start()
     {
-        enemysensorscript = enemysensor.GetComponent<EnemySensor>();
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        float horizontalinput = Input.GetAxisRaw("Horizontal");
-        float verticalinput = Input.GetAxisRaw("Vertical");
+        float UpDowninput = Input.GetAxisRaw("Horizontal");
+        float LeftRightinput = Input.GetAxisRaw("Vertical");
+
+        print("Left Right movement" + UpDowninput);
+        print("\nUp down movement" + LeftRightinput);
         
         if (Playerbody.velocity.x > 30f)
         {
-            horizontalinput = 0;
-            verticalinput = 0;
+            UpDowninput = 0;
+            LeftRightinput = 0;
         }
 
-        direction = new Vector3(horizontalinput, 0f, verticalinput);
+        if (UpDowninput > 0.1f || UpDowninput < -0.1f){
+            LeftRightinput = 0;
+        }
+        else if (LeftRightinput > 0.1f || LeftRightinput < -0.1f){
+            UpDowninput = 0;
+        }
+        direction = new Vector3(UpDowninput, 0f, LeftRightinput);
         targetangle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
 
-        if (enemysensorscript.cameralockedon)
-        {
-            directiontoenemy = enemysensorscript.CurrentlyLockedTarget.transform.position - transform.position;
-            directiontoenemy.y = 0;
+        directiontoenemy = (Enemy.position - transform.position).normalized;
+        directiontoenemy.y = 0;
+        
 
-            player_rotation = Quaternion.LookRotation(directiontoenemy);
-            transform.rotation = player_rotation;
-        }
-
-        if (direction.magnitude >= 0.1f && enemysensorscript.cameralockedon)
-        {
+        player_rotation = Quaternion.LookRotation(directiontoenemy);
+        transform.rotation = player_rotation;
+        if (direction.magnitude >= 0.1f){
+            Vector3 movedirection = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;
             
-
-            Vector3 movedirection = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;
-            transform.position = new Vector3(transform.position.x + (movedirection.x * PlayerSpeed * Time.deltaTime), transform.position.y, transform.position.z);
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (movedirection.z * PlayerSpeed * Time.deltaTime));
-        }
-        else if (direction.magnitude >= 0.1f)
-        {
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetangle, ref turnsmoothvelocity, TurnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 movedirection = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;
             transform.position = new Vector3(transform.position.x + (movedirection.x * PlayerSpeed * Time.deltaTime), transform.position.y, transform.position.z);
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (movedirection.z * PlayerSpeed * Time.deltaTime));
         }
-
+        
+        
     }
 
 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Launcher")
+        if (collision.gameObject.tag == "normalhit")
         {
             Playerbody.AddForce(new Vector3(2000, 0, 0));
             
