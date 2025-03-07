f3291d7978b552329de54ef6f94328e017c5aa51
Local Sugarcoder <152346785+LocalSugarcoater@users.noreply.github.com>
2025-03-07 08:18:12 -0500

    Fixed movement
    Movement is fixed sidewalk works and  back and forward movement also works. Moving on to adding animations.

--- Assets/Old scripts/Player Movement.cs
+++ Assets/Old scripts/Player Movement.cs
@@ -34,12 +34,6 @@ public class PlayerMovement : MonoBehaviour
             LeftRightinput = 0;
         }
 
-        if (UpDowninput > 0.1f || UpDowninput < -0.1f){
-            LeftRightinput = 0;
-        }
-        else if (LeftRightinput > 0.1f || LeftRightinput < -0.1f){
-            UpDowninput = 0;
-        }
         direction = new Vector3(UpDowninput, 0f, LeftRightinput);
         targetangle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
 
@@ -49,12 +43,22 @@ public class PlayerMovement : MonoBehaviour
 
         player_rotation = Quaternion.LookRotation(directiontoenemy);
         transform.rotation = player_rotation;
-        if (direction.magnitude >= 0.1f){
+        if (UpDowninput > 0.1f || UpDowninput < -0.1f){
+            Vector3 forwarddirection = transform.forward;
+            transform.position += forwarddirection * UpDowninput * PlayerSpeed* Time.deltaTime;
+        }
+        else if (LeftRightinput > 0.1f || LeftRightinput < -0.1f){
+            Vector3 rightdirection = transform.right;
+            LeftRightinput = LeftRightinput * -1f;
+            transform.position += rightdirection * LeftRightinput * PlayerSpeed* Time.deltaTime;
+        }
+        /*if (direction.magnitude >= 0.1f){
             Vector3 movedirection = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;
             
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (movedirection.z * PlayerSpeed * Time.deltaTime));
             transform.position = new Vector3(transform.position.x + (movedirection.x * PlayerSpeed * Time.deltaTime), transform.position.y, transform.position.z);
-        }
+
+        }*/
         
         
     }
